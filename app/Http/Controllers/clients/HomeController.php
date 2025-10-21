2ef8f0f420a0d41a849266d8d3e03f770706af3c
thanhlam200496 <thanhlam200496@gmail.com>
2025-10-22 02:30:56 +0700

    ve giao dien cu

--- app/Http/Controllers/clients/HomeController.php
+++ app/Http/Controllers/clients/HomeController.php
@@ -15,43 +15,12 @@ class HomeController extends Controller
     {
         return view('client_movie.layouts.default');
     }
-    // public function home(Request $request)
-    // {
-    //     $moviesPopular = Movie::with(['categories', 'episodes'])->where(['status' => 'Public'])->orderBy('views', 'DESC')->take(10)->get();
-    //     // dd($moviesPopular);
-
-    //     $moviesNew = Movie::with('categories')->where(['status' => 'Public'])->orderBy('created_at', 'DESC')->take(10)->get();
-    //     $moviesHotInYear = Movie::with('categories')
-    //         ->where([
-    //             'status' => 'Public',
-    //             'release_year' => Carbon::now()->year
-    //         ])
-    //         ->orderBy('views', 'DESC')
-    //         ->take(10)
-    //         ->get();
-    //     $favorites = Favorite::all();
-    //     if (Auth::check() && isset(Auth::user()->id)) {
-    //         $favoriteMovies = Movie::join('favorites', 'movies.id', '=', 'favorites.movie_id')
-    //             ->where('favorites.user_id', Auth::user()->id)
-    //             ->select('movies.*', 'favorites.created_at as favorited_at')
-    //             ->orderBy('favorites.created_at', 'DESC')
-    //             ->get();
-    //     } else {
-    //         $favoriteMovies = '';
-    //     }
-
-
-    //     return view('client_movie.home', compact('moviesPopular', 'favorites', 'favoriteMovies', 'moviesNew', 'moviesHotInYear'));
-    // }
-
-    // giao dien moi
     public function home(Request $request)
     {
         $moviesPopular = Movie::with(['categories', 'episodes'])->where(['status' => 'Public'])->orderBy('views', 'DESC')->take(10)->get();
         // dd($moviesPopular);
 
         $moviesNew = Movie::with('categories')->where(['status' => 'Public'])->orderBy('created_at', 'DESC')->take(10)->get();
-        $trendingMovie = Movie::with('categories')->where(['status' => 'Public'])->orderBy('created_at', 'DESC')->take(10)->get();
         $moviesHotInYear = Movie::with('categories')
             ->where([
                 'status' => 'Public',
@@ -72,6 +41,37 @@ class HomeController extends Controller
         }
 
 
-        return view('client_movie.home', compact('moviesPopular','trendingMovie', 'favorites', 'favoriteMovies', 'moviesNew', 'moviesHotInYear'));
+        return view('client_movie.home', compact('moviesPopular', 'favorites', 'favoriteMovies', 'moviesNew', 'moviesHotInYear'));
     }
+
+    // // giao dien moi
+    // public function home(Request $request)
+    // {
+    //     $moviesPopular = Movie::with(['categories', 'episodes'])->where(['status' => 'Public'])->orderBy('views', 'DESC')->take(10)->get();
+    //     // dd($moviesPopular);
+
+    //     $moviesNew = Movie::with('categories')->where(['status' => 'Public'])->orderBy('created_at', 'DESC')->take(10)->get();
+    //     $trendingMovie = Movie::with('categories')->where(['status' => 'Public'])->orderBy('created_at', 'DESC')->take(10)->get();
+    //     $moviesHotInYear = Movie::with('categories')
+    //         ->where([
+    //             'status' => 'Public',
+    //             'release_year' => Carbon::now()->year
+    //         ])
+    //         ->orderBy('views', 'DESC')
+    //         ->take(10)
+    //         ->get();
+    //     $favorites = Favorite::all();
+    //     if (Auth::check() && isset(Auth::user()->id)) {
+    //         $favoriteMovies = Movie::join('favorites', 'movies.id', '=', 'favorites.movie_id')
+    //             ->where('favorites.user_id', Auth::user()->id)
+    //             ->select('movies.*', 'favorites.created_at as favorited_at')
+    //             ->orderBy('favorites.created_at', 'DESC')
+    //             ->get();
+    //     } else {
+    //         $favoriteMovies = '';
+    //     }
+
+
+    //     return view('client_movie.home', compact('moviesPopular','trendingMovie', 'favorites', 'favoriteMovies', 'moviesNew', 'moviesHotInYear'));
+    // }
 }
